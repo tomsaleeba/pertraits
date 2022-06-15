fafc219f72e0634699502d05e8c1aeb28a4f1416
Tom Saleeba <tom.saleeba@gmail.com>
2022-06-15 09:39:53 -0600

    test: fix broken test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const someElement = screen.getByText(/Todo List/i)
+  const someElement = screen.getByText(/Pertraits/i)
   expect(someElement).toBeInTheDocument()
 })
